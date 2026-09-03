26e4b689a75b64e4678895c7fded2a3aa966e0fc
Egor <egor.gusev2018@gmail.com>
2026-09-03 17:16:53 -0400

    learned about initialization and creating first functions

--- 01-Basic Elements.cpp
+++ 01-Basic Elements.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 
+int five() {
+        return 5;
+    }
+
+int multiply(int a, int b){
+    return a*b;
+}    
+
 int main()
 {
-    std::cout << "Here is some text.\n";
+    // std::cout << 5 << '\n';         //print the literal number '5'
+    // std::cout << -6.7 << '\n';     //print the literal number '-6,7'
+    // std::cout << 'H' << '\n';       //print the literal character 'H'
+    // std::cout << "Hello"<< '\n';   //print the literal text 'Hello'
 
-    std::cout << "Press Enter to exit...";
+    // int x;                //define a variable named x (of type int)
+
+    // double width;         //define a variable named width, of type double
+    // width = 5;            //assignment of value 5 into variable width
+    // std::cout << width << '\n';
+    // width = 7;
+    // std::cout << width << '\n';
+    // int height {6};         //define a variable height and initialize with inital value 6!!!PREFERRED!!
+    // std::cout << height << '\n';
+
+
+    // five() is a function that returns the value 5
+    
+    int e {five()};
+    std::cout << e << '\n';
+
+    
+
+
     std::cin.get();
+
+    
 
     return 0;
 }
